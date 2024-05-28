038e11c05bac9b978cd47b36aa20ece4c91f0c62
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-05-28 07:12:52 +0300

    build home page UI

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import Carousel from "../../components/Carousel";
 import ProductsList from "./components/ProductsList";
+import { images } from "./data";
 import styles from "./styles.module.css";
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
+      <Carousel images={images} />
       <ProductsList />
     </div>
   );
