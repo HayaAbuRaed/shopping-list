815e2f3ec484f0e03940ae69ce7152c417493a6a
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-05-28 19:26:14 +0300

    add checkout dialog and implement fake checkout functionality

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,40 +1,54 @@
 import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartProductsContext from "../../context/CartProductsContext/CartProductsContext";
 import styles from "./Cart.module.css";
 import CartItem from "./components/CartItem";
 import EmptyCart from "./components/EmptyCart";
 import { calculateTotal } from "./util";
+import { CheckoutDialog } from "../../containers/Dialogs";
 
 const Cart = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   const { cartProducts } = useContext(CartProductsContext);
 
   const total = calculateTotal(cartProducts);
 
   if (total <= 0) return <EmptyCart />;
 
+  const handleCheckoutClick = () => {
+    setIsDialogOpen(true);
+  };
+
   return (
-    <div className={styles.cart}>
-      <ul className={styles.cartList}>
-        {cartProducts.map((item) => (
-          <CartItem key={item.id} product={item} />
-        ))}
-      </ul>
+    <>
+      <div className={styles.cart}>
+        <ul className={styles.cartList}>
+          {cartProducts.map((item) => (
+            <CartItem key={item.id} product={item} />
+          ))}
+        </ul>
 
-      <div className={styles.totalBox}>
-        <h4 className={styles.total}>Total: ${total}</h4>
+        <div className={styles.totalBox}>
+          <h4 className={styles.total}>Total: ${total}</h4>
 
-        <button
-          className={`${styles.checkoutButton} ${
-            !(total > 0) && styles.disabled
-          }`}
-        >
-          <FontAwesomeIcon icon={faCircleCheck} />
-          Checkout
-        </button>
+          <button
+            className={`${styles.checkoutButton} ${
+              !(total > 0) && styles.disabled
+            }`}
+            onClick={handleCheckoutClick}
+          >
+            <FontAwesomeIcon icon={faCircleCheck} />
+            Checkout
+          </button>
+        </div>
       </div>
-    </div>
+
+      {isDialogOpen && (
+        <CheckoutDialog onClose={() => setIsDialogOpen(false)} />
+      )}
+    </>
   );
 };
 
